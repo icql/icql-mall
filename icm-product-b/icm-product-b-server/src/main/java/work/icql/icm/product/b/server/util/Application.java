4a203a3e0f07c6d6abde621e949a3cbe08438e6f
icql <icqlchen@qq.com>
2023-10-22 10:00:00 +0800

    chore: 调整逻辑

---
+++ icm-product-b/icm-product-b-server/src/main/java/work/icql/icm/product/b/server/util/Application.java
@@ -0,0 +1,8 @@
+package work.icql.icm.product.b.server.util;
+
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+//        PdfMergeUtils.merge("C:\\Users\\Casstime\\Desktop\\test", "C:\\Users\\Casstime\\Desktop\\test11");
+    }
+}
